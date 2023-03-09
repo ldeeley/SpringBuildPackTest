c1ccc8517471853073ceca45c18e13b2265b7bdd
ldeeley <lester.deeley@yahoo.com>
2023-03-09 11:03:59 +0000

    Initial commit

---
+++ src/main/java/com/example/springbuildpacktest/SpringBuildPackTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbuildpacktest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBuildPackTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBuildPackTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbuildpacktest/SpringBuildPackTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbuildpacktest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBuildPackTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
